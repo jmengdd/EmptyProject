dc72848b4167fb952ad586de881c5af7aaed7c87
Jimmy <jimmy@doordash.com>
2017-04-16 19:37:44 -0700

    add butter kinfe

--- app/src/main/java/com/jimmy/project/MainActivity.java
+++ app/src/main/java/com/jimmy/project/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,4 +28,5 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
 }
